c32d5a193817dbd0674e221e8db78740b00e6d99
Sam <samnewell91@gmail.com>
2023-04-05 19:22:23 +0100

    Replaced IF function with a ternary operator
    I replaced the lengthy if function that I had to check  whether or not a username had been entered with a simple ternary function that serves the same purpose.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Defining the Variables 
 let userName = prompt("Enter Your Name");
-let userNameFinal = "";
+let userNameFinal = (userName == null || userName == "") ? "Holder of the Magical Ball"
+    : userName.charAt(0).toUpperCase() + userName.slice(1);
+console.log(`Welcome ${userNameFinal}, type ask() if you seek the truth`)
+let userQuestion = "";
 
 // The possible responses for the eight ball
 let answers = ["It is certain",
@@ -14,22 +17,23 @@ let answers = ["It is certain",
     "Ask again later",
     "Don't count on it",
     "For certain",
-    ]
+]
 
-// Checks if a username has been entered and if so it returns it in Capital Case
+// Checks if a username has been entered and if so it returns it in Capital Case - If Statement 
 
-if (userName == null) {
-    console.log(`Hello, type ask() to ask the magical 8 ball a question`);
-    userNameFinal = "Holder of the magical ball";
-} else if (userName == "") {
-    console.log(`Hello, type ask() to ask the magical 8 ball a question`);
-    userNameFinal = "Holder of the magical ball";
-} else {
-    userNameFinal = userName.charAt(0).toUpperCase() + userName.slice(1);
-    console.log(`Hello ${userNameFinal}, type ask() to ask the magical 8 ball a question`);
-}
+// if (userName == null) {
+//     console.log(`Hello, type ask() to ask the magical 8 ball a question`);
+//     userNameFinal = "Holder of the magical ball";
+// } else if (userName == "") {
+//     console.log(`Hello, type ask() to ask the magical 8 ball a question`);
+//     userNameFinal = "Holder of the magical ball";
+// } else {
+//     userNameFinal = userName.charAt(0).toUpperCase() + userName.slice(1);
+//     console.log(`Hello ${userNameFinal}, type ask() to ask the magical 8 ball a question`);
+// }
 
-let userQuestion = "";
+
+
 // Asking a question
 function ask() {
     userQuestion = prompt('What do you want to know');
